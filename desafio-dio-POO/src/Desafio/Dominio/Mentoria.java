b93f13a59db556a4c226db84226081d8356fb4da
Rick Mamona <rickmamona07@gmail.com>
2024-09-08 08:31:45 -0300

    Criacao classe Conteudo

--- desafio-dio-POO/src/Desafio/Dominio/Mentoria.java
+++ desafio-dio-POO/src/Desafio/Dominio/Mentoria.java
@@ -3,25 +3,16 @@ package Desafio.Dominio;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Mentoria {
+public class Mentoria extends Conteudo {
 
-    private String titulo;
-    private String descricao;
     private LocalDate data;
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
     public Mentoria() {
-    }
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
     public LocalDate getData() {
         return data;
@@ -34,7 +25,8 @@ public class Mentoria {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String formattedDate = data.format(formatter);
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + formattedDate + "]";
+        return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + formattedDate + "]";
     }
+
 
 }
